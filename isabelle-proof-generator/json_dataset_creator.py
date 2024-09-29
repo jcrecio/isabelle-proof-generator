ad285ed87efc1929219dde8db5f38a4dc0aa7970
Juan Carlos Recio Abad <juan.carlos.recio.abad@gmail.com>
2024-09-30 01:05:27 +0200

    Refactor and documentation

--- isabelle-proof-generator/json_dataset_creator.py
+++ isabelle-proof-generator/json_dataset_creator.py
@@ -27,34 +27,70 @@ def get_lemma_proof_for_file(extraction_file_path: str):
 
     for index in range(0, last_lemma_index):
         lemma_step = translations[index][1]
-        theorem_statement = f'{theorem_statement}\\n{lemma_step} '
+        theorem_statement = f'''{theorem_statement}
+{lemma_step}'''
 
     for index in range(last_lemma_index + 1, len(translations)):
         proof_step = translations[index][1]
-        proof = f'{proof}\\n{proof_step} '
+        proof = f'''{proof}
+{proof_step} '''
     
     return [theorem_statement, proof]
 
-def get_dataset_items_from_extraction_folder(extraction_folder_path: str):
+def read_abstract_from_tex_file(file_path: str):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+        start_tag = '\\begin{abstract}'
+        end_tag = '\\end{abstract}'
+        start_index = content.find(start_tag)
+        end_index = content.find(end_tag)
+        if start_index != -1 and end_index != -1:
+            start_index += len(start_tag)
+            return content[start_index:end_index].strip()
+        
+    return 'NO_CONTEXT'
+
+def get_problem_context(afp_folder):
+    abstract_tex_path = os.path.join(afp_folder, 'document', 'abstract.tex')
+    if os.path.exists(abstract_tex_path):
+        return read_abstract_from_tex_file(abstract_tex_path)
+    
+    root_tex_path = os.path.join(afp_folder, 'document', 'root.tex')
+    if os.path.exists(root_tex_path):
+        return read_abstract_from_tex_file(root_tex_path)
+
+    return 'NO_CONTEXT'
+
+def get_dataset_items(afp_folder: str, extraction_folder_path: str, with_context: bool):
     proofs = []
     for file_name in os.listdir(extraction_folder_path):
         if not file_name.endswith(".json"):
             continue
 
+        context = 'NO_CONTEXT'
+        if with_context == 'True':
+            context = get_problem_context(afp_folder)
+
         file_path = os.path.join(extraction_folder_path, file_name)
         if os.path.isfile(file_path):
             [theorem_statement, proof] = get_lemma_proof_for_file(file_path)
             if theorem_statement != '' and proof != '':
-                proofs.append({'theorem_statement': theorem_statement, 'proof': proof})
+                proofs.append({'context': context, 'theorem_statement': theorem_statement, 'proof': proof})
 
     return proofs
 
 problems_folder = os.getenv('PROBLEMS_FOLDER')
+afp_folder = os.getenv('AFP_FOLDER')
 output_file = os.getenv('OUTPUT_FILE_DATASET')
+with_context = os.getenv('WITH_CONTEXT')
 
-proofs = [get_dataset_items_from_extraction_folder(os.path.join(problems_folder, str(folder))) 
-          for folder in os.listdir(problems_folder) 
-                   if os.path.isdir(os.path.join(problems_folder, str(folder)))]
-flattened_proofs = [element for sublist in proofs for element in sublist]
+proofs = []
+for item in os.listdir(problems_folder):
+    proofs = proofs + get_dataset_items(os.path.join(afp_folder, str(item)),os.path.join(problems_folder, str(item)), with_context)
 
-write_json_file(output_file, { 'proofs': flattened_proofs })
+# proofs = [get_dataset_items(os.path.join(afp_folder, str(folder)),
+#                             os.path.join(problems_folder, str(folder)), with_context) 
+#           for folder in os.listdir(problems_folder) 
+#                    if os.path.isdir(os.path.join(str(folder)))]
+
+write_json_file(output_file, { 'proofs': proofs })
--- isabelle-proof-generator/proofs_to_mistral.py
+++ isabelle-proof-generator/proofs_to_mistral.py
@@ -1,10 +1,18 @@
 import json
 import sys
 
-PROMPT_TEMPLATE = 'You are now an specialized agent to infer proofs for theorem statements or lemmas written in Isabelle/HOL. You are going to receive instructions of what you need to infer, and you will also receive some context and the corresponding theorem statement or lemma.[INST]Infer a proof for the following Isabelle/HOL theorem statement/s: {theorem_statement}[/INST]{proof}'
+PROMPT_TEMPLATE_QUESTION_ANSWER_WITH_CONTEXT = 'You are now an specialized agent to infer proofs for problems, theorem statements or lemmas written in Isabelle/HOL. You are going to receive instructions of what you need to infer, and you will also receive some context and the corresponding problem, theorem statement or lemma.[INST]Given the problem context "{context}". Infer a proof for the following Isabelle/HOL theorem statement/s: {theorem_statement}[/INST]{proof}'
+PROMPT_TEMPLATE_QUESTION_ANSWER = 'You are now an specialized agent to infer proofs for problems, theorem statements or lemmas written in Isabelle/HOL. You are going to receive instructions of what you need to infer, and you will also receive some context and the corresponding problem, theorem statement or lemma.[INST]Infer a proof for the following Isabelle/HOL theorem statement/s: {theorem_statement}[/INST]{proof}'
+NO_CONTEXT = 'NO_CONTEXT'
 
-def map_prompt(theorem_statement, proof):
-    return PROMPT_TEMPLATE.replace('{theorem_statement}', theorem_statement).replace('{proof}', proof)
+def map_prompt(context, theorem_statement, proof):
+    if context == NO_CONTEXT: 
+        return PROMPT_TEMPLATE_QUESTION_ANSWER.replace('{theorem_statement}', theorem_statement).replace('{proof}', proof)
+    
+    return PROMPT_TEMPLATE_QUESTION_ANSWER_WITH_CONTEXT \
+        .replace('{context}', context) \
+        .replace('{theorem_statement}', theorem_statement) \
+        .replace('{proof}', proof)
 
 def proofs_jsonl_to_mistral(input_file, output_file):
     with open(input_file, 'r') as infile:
@@ -12,15 +20,16 @@ def proofs_jsonl_to_mistral(input_file, output_file):
         problems = json_data.get('proofs')
         with open(output_file, 'w') as outfile:
             for problem in problems:
+                context = problem.get('context')
                 theorem_statement = problem.get('theorem_statement')
                 proof = problem.get('proof')
-                json.dump({'proof': map_prompt(theorem_statement, proof)}, outfile)
+                json.dump({'proof': map_prompt(context, theorem_statement, proof)}, outfile)
                 outfile.write('\n')
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python proofs_jsonl_to_mistral.py input_file.jsonl output_file.json")
+        print("Usage: python proofs_to_mistral.py input_file.json output_file.jsonl")
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
