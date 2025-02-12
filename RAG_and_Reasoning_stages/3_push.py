7c3dde67d7910e241e1e1e950e1c97cc14e421eb
JC Recio <juan.carlos.recio.abad@gmail.com>
2025-02-13 00:43:33 +0100

    push c and normal

--- RAG_and_Reasoning_stages/3_push.py
+++ RAG_and_Reasoning_stages/3_push.py
@@ -2,12 +2,18 @@ from unsloth import FastLanguageModel
 from peft import PeftModel
 from huggingface_hub import login
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 hf_token = os.getenv("HF_TOKEN")
+WITH_CONTEXT = os.getenv("WITH_CONTEXT")
+
 login(hf_token)
 
 base_model_path = "unsloth/DeepSeek-R1-Distill-Llama-8B"
-lora_model_path = "jcrecio/Remath-v0.1"
+lora_model_path = (
+    "jcrecio/Remath-v0.1-c if WITH_CONTEXT == 'True' else jcrecio/Remath-v0.1"
+)
 
 base_model, tokenizer = FastLanguageModel.from_pretrained(
     model_name=base_model_path, max_seq_length=4096, dtype="bfloat16", load_in_4bit=True
@@ -16,9 +22,13 @@ base_model, tokenizer = FastLanguageModel.from_pretrained(
 model = PeftModel.from_pretrained(base_model, lora_model_path)
 model = model.merge_and_unload()
 
-repo_id = "jcrecio/Remath-v0.1"
+repo_id = lora_model_path
 
-merged_model_path = "jcrecio/Remath-v0.1-merged"
+merged_model_path = (
+    "jcrecio/Remath-v0.1-c-merged"
+    if WITH_CONTEXT == "True"
+    else "jcrecio/Remath-v0.1-merged"
+)
 model.save_pretrained(merged_model_path)
 tokenizer.save_pretrained(merged_model_path)
 model.save_pretrained_merged(
